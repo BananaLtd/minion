2ba49027494c9f32afb3bad94ca09f1d374f27a3
Steffen Schröder <steffen@schroeder-blog.de>
2013-08-08 10:58:39 +0200

    add version to docs

--- lib/mix/publish.ex
+++ lib/mix/publish.ex
@@ -7,12 +7,15 @@ defmodule Mix.Tasks.Docs.Publish do
   Generates documentation and commits to gh-pages branch 
   """
   def run(_) do
+    Mix.Project.get!
+    version = Mix.Project.config[:version]
+
     System.cmd "git co master"
     System.cmd "mix do deps.get, docs"
-    System.cmd "mv -R docs/ /tmp/minion-docs/"
+    System.cmd "mv docs/ /tmp/minion-docs-#{version}"
     System.cmd "git co gh-pages"
-    System.cmd "rm -rf docs/"
-    System.cmd "mv -R /tmp/minion-docs/ docs/"
+    System.cmd "rm -rf docs/#{version}/"
+    System.cmd "mv /tmp/minion-docs-#{version}/ docs/#{version}"
     System.cmd "git add docs"
     System.cmd "git commit -m 'Updated docs'"
     System.cmd "git co master"
